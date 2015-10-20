af8ac9c3ff6958a84c8c57aaf7b7b07e7f6330c6
Zetagon <leo.ericson@yahoo.se>
2015-10-20 16:07:04 +0200

    more code

--- lib/ex48/lexicon.rb
+++ lib/ex48/lexicon.rb
@@ -1,24 +1,39 @@
-class Lexicon
-	def scan(string)
+module Lexicon
+	def convert_number(object)
+		begin
+			return Integer(object)
+		rescue
+			return nil
+		end
+	end
+	
+	def Lexicon.scan(string)
 		words = [['direction','north'],
 		['direction','south'],
-		['direction','east']]
+		['direction','east'],
+		['verb','go'],
+		['verb','kill'],
+		['verb','eat'],
+		['stop','the'],
+		['stop','in'],
+		['stop','of'],
+		['noun','princess'],
+		['noun','bear']]
 		ary=[]
 		string = string.split
 		
-		string.each do |x|
-			words.each do |a|
-				a.each do |b,c|
-					if x == c
-					return a
+		string.each do |a|
+			if convert_number(string).nil?
+				words.each do |b|
+					if b[1] == a
+						ary.push(b)
 					end
 				end
+			else
+				ary = [['number',convert_number(string)]]
 			end
 		end
 		
-
+	return ary
 	end
 end
-
-lex = Lexicon.new
-lex.scan('north')
